fcec6a3bd2c6819d25af8ffed5c7a871c352ee71
tuandhtl <tuandhtl@gmail.com>
2024-09-09 11:26:27 +0700

    snack

--- t1.py
+++ t1.py
@@ -1,6 +1,8 @@
 hungry = input("are u hungry?")
 if hungry=="yes":
    print("eat pho")
+   print("eat fried")
+   print("eat snack")
 else:
    thirsty = input("are you thirsty?")
    if thirsty=="yes":
